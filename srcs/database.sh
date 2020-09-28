23508ab25b2b951dd6c5fb3c3f876974c9c95059
Diord <Diord@DESKTOP-6B09NPQ>
2020-09-28 20:27:44 +0300

    Done

--- srcs/database.sh
+++ srcs/database.sh
@@ -1,4 +1,3 @@
 echo "CREATE DATABASE Wordpress;"| mysql -u root --skip-password
-echo "GRANT ALL PRIVILEGES ON Wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;" mysql -u root --skip-password
-echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
-echo "set password for root@localhost=password('admin');"| mysql -u root --skip-password 
+echo "GRANT ALL PRIVILEGES ON Wordpress.* TO 'root'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "SET PASSWORD FOR root@localhost=password('admin');"| mysql -u root --skip-password
